f364acc052ca688984d040102be1dc4be27a6508
unknown <brajshekar7@gmail.com>
2025-06-10 14:27:58 +0530

    Updated TestNG Browsers

--- src/main/java/utils/BrowserSelection.java
+++ src/main/java/utils/BrowserSelection.java
@@ -2,6 +2,7 @@ package main.java.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -13,9 +14,9 @@ public class BrowserSelection {
 			WebDriverManager.chromedriver().setup();
 			return new ChromeDriver();
 		}
-		else if(browser.equalsIgnoreCase("firefox")){
-			WebDriverManager.firefoxdriver().setup();
-			return new FirefoxDriver();
+		else if(browser.equalsIgnoreCase("edge")){
+			WebDriverManager.edgedriver().setup();
+			return new EdgeDriver();
 		}
 		throw new IllegalArgumentException("Browser not supported");
 	}
